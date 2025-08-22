2f4503656d7075958997a7567df2c296c58f3e14
milkwxter <ethan.r.bates2003@gmail.com>
2025-08-22 12:09:44 -0500

    Fixes, basically addon is done now.
    - Players can only equip 1 vest per round.
    - Vest removes itself on death.
    - Vest removes itself on a new round.
    - Description notifies you if you can equip the armor or not.

--- ttt2_milkwaters_armor/gamemodes/terrortown/entities/entities/ent_armor_vest.lua
+++ ttt2_milkwaters_armor/gamemodes/terrortown/entities/entities/ent_armor_vest.lua
@@ -1,7 +1,8 @@
 if SERVER then
     AddCSLuaFile()
 	
-	util.AddNetworkString("EquipArmorVest")
+	util.AddNetworkString("EquipArmorVestOnPlayer")
+	util.AddNetworkString("RemoveArmorVestOnPlayer")
 end
 
 DEFINE_BASECLASS("ttt_base_placeable")
@@ -41,6 +42,9 @@ if SERVER then
         -- make sure the guy using the item exists
         if not IsValid(ply) or not ply:IsPlayer() or not ply:IsActive() then return end
 		
+		-- make sure he doesnt have armor already
+		if ply:GetArmor() >= 20 then return end
+		
         -- make sound for feedback
         self:EmitSound(soundPickup)
 		
@@ -48,7 +52,7 @@ if SERVER then
 		ply:GiveArmor(20)
 		
 		-- blah blah blah
-		net.Start("EquipArmorVest")
+		net.Start("EquipArmorVestOnPlayer")
         net.WriteEntity(ply)
         net.WriteString("models/player/armor_module3m/module3m.mdl")
         net.Send(player.GetAll())
@@ -56,6 +60,22 @@ if SERVER then
         -- delete item so he cant use it again
         self:Remove()
     end
+	
+	-- remove armor on death
+	hook.Add("PostPlayerDeath", "RemoveVestHookOnDeath", function(ply)
+		net.Start("RemoveArmorVestOnPlayer")
+        net.WriteEntity(ply)
+        net.Send(player.GetAll())
+	end)
+	
+	-- remove armor on round preparing (most immersive)
+	hook.Add("TTTPrepareRound", "RemoveVestHookOnRoundPrep", function()
+		for _, ply in ipairs(player.GetAll()) do
+			net.Start("RemoveArmorVestOnPlayer")
+			net.WriteEntity(ply)
+			net.Send(player.GetAll())
+		end
+	end)
 end
 
 if CLIENT then
@@ -69,7 +89,7 @@ if CLIENT then
 	
 	local vestData = {}
 
-	net.Receive("EquipArmorVest", function()
+	net.Receive("EquipArmorVestOnPlayer", function()
 		local ply = net.ReadEntity()
 		local modelPath = net.ReadString()
 
@@ -81,6 +101,21 @@ if CLIENT then
 		}
 
 		vestData[ply].model:SetNoDraw(true)
+	end)
+	
+	net.Receive("RemoveArmorVestOnPlayer", function()
+		local ply = net.ReadEntity()
+		local modelPath = net.ReadString()
+
+		if not IsValid(ply) then return end
+		
+		print("Trying to remove vest from " .. ply:Nick())
+		
+		if vestData[ply] and IsValid(vestData[ply].model) then
+			vestData[ply].model:Remove()
+		end
+
+		vestData[ply] = nil
 	end)
 
     ---
@@ -97,7 +132,7 @@ if CLIENT then
     end
 
     -- handle looking at item
-    hook.Add("TTTRenderEntityInfo", "HUDDrawTargetIDArmor", function(tData)
+    hook.Add("TTTRenderEntityInfo", "HUDDrawTargetIDArmorVest", function(tData)
         local client = LocalPlayer()
         local ent = tData:GetEntity()
 
@@ -121,6 +156,10 @@ if CLIENT then
         tData:SetSubtitle("Press [E] to equip the armor vest.")
 
         tData:AddDescriptionLine("Armor reduces incoming damage. Unless you get headshot.")
+		
+		if client:GetArmor() > 0 then
+            tData:AddDescriptionLine("You cannot wear more than 1 armor vest!", COLOR_RED)
+        end
     end)
 	
 	-- handle drawing it on the player
@@ -141,13 +180,5 @@ if CLIENT then
 		vest:SetRenderAngles(ang)
 		vest:SetModelScale(1.25)
 		vest:DrawModel()
-	end)
-	
-	-- handle death
-	hook.Add("PostPlayerDeath", "RemoveCustomVest", function(ply)
-		if vestData[ply] and IsValid(vestData[ply].model) then
-			vestData[ply].model:Remove()
-		end
-		vestData[ply] = nil
 	end)
 end
